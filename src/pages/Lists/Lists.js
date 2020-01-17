1e94d257413b2161750678c461ed69af64a3f027
hjhun-Macmini <hjhun0504@gmail.com>
2020-01-17 21:36:43 +0900

    Add fake data rendering

--- src/pages/Lists/Lists.js
+++ src/pages/Lists/Lists.js
@@ -3,21 +3,54 @@ import { ListEntry, ListHead, SearchBar, WriteButton } from "./components";
 import "./Lists.css";
 
 export default class Lists extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      posts: [
+        {
+          id: 5,
+          author: "고양이맨",
+          title: "우리집 고양이 보고가~",
+          createdAt: "2020/01/17"
+        },
+        {
+          id: 3,
+          author: "🐶",
+          title: "솔직히 고양이들 별로아님?",
+          createdAt: "2020/01/17"
+        },
+        {
+          id: 2,
+          author: "야옹",
+          title: "오늘 고양이 주움",
+          createdAt: "2020/01/17"
+        },
+        {
+          id: 1,
+          author: "노캣",
+          title: "나만 고양이 없어",
+          createdAt: "2020/01/17"
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <>
         <table className="board">
           <ListHead />
           <tbody>
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
-            <ListEntry />
+            {this.state.posts.map(post => {
+              return <ListEntry key={post.id} post={post} />;
+            })}
           </tbody>
         </table>
 
--- src/pages/Lists/components/ListEntry.js
+++ src/pages/Lists/components/ListEntry.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./ListEntry.css";
 
-const ListEntry = () => {
+const ListEntry = ({ post }) => {
   return (
     <tr className="list">
-      <td className="list__id">123</td>
+      <td className="list__id">{post.id}</td>
       <td className="list__title">
         <i className="list__icon fas fa-comment-dots fa-flip-horizontal"></i>
-        우리집 냥이 보고 가~
+        {post.title}
       </td>
-      <td className="list__author">고양이맨</td>
-      <td className="list__createdAt">2020/01/17</td>
+      <td className="list__author">{post.author}</td>
+      <td className="list__createdAt">{post.createdAt}</td>
     </tr>
   );
 };
