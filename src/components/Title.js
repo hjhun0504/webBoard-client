8242fab9533d04b41847c833d65f14448adfaded
hjhun-Macmini <hjhun0504@gmail.com>
2020-01-18 19:16:51 +0900

    Add title click event: go to main page
    add css styling links

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Title.css";
+import { Link } from "react-router-dom";
 
 const Title = () => {
-  return <header className="title">야옹이 게시판</header>;
+  return (
+    <>
+      <Link to="/">
+        <header className="title">야옹이 게시판</header>
+      </Link>
+    </>
+  );
 };
 
 export default Title;
