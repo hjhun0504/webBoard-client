29d8e199cf343d0ab20e94c3c74071ef21c6fd7e
hjhun-Macmini <hjhun0504@gmail.com>
2020-01-19 23:23:10 +0900

    Apply write page css

--- src/pages/Write/Write.js
+++ src/pages/Write/Write.js
@@ -8,23 +8,33 @@ export default class Write extends Component {
     return (
       <div className="write-wrapper">
         <div>
-          <input type="text" placeholder="닉네임" />
-          <input type="text" placeholder="비밀번호" />
+          <input className="write__author" type="text" placeholder="닉네임" />
+          <input
+            className="write__password"
+            type="text"
+            placeholder="비밀번호"
+          />
         </div>
         <div>
-          <input type="text" placeholder="제목을 입력해 주세요." />
+          <input
+            className="write__title"
+            type="text"
+            placeholder="제목을 입력해 주세요."
+          />
         </div>
-        <div>
-          <div>※ 쉬운 비밀번호를 입력하면 타인의 수정, 삭제가 쉽습니다.</div>
-          <div>
-            음란물, 차별, 비하, 혐오 및 초상권, 저작권 침해 게시물은 민,
+        <div className="write__disclaimer">
+          <div className="write__disclaimer__content">
+            ※ 쉬운 비밀번호를 입력하면 타인의 수정, 삭제가 쉽습니다.
+          </div>
+          <div className="write__disclaimer__content">
+            ※ 음란물, 차별, 비하, 혐오 및 초상권, 저작권 침해 게시물은 민,
             형사상의 책임을 질 수 있습니다. [저작권법 안내] [게시물 활용 안내]
           </div>
         </div>
         <div>
           <textarea className="write__box"></textarea>
         </div>
-        <div>
+        <div className="write__button-box button-box">
           <Link to="/">
             <Button title="취소" css="button--grey"></Button>
           </Link>
