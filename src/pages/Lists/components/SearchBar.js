9beafa66322a59d6cf3aedb8c4df3e9dd2deb423
hjhun-Macmini <hjhun0504@gmail.com>
2020-01-21 22:57:20 +0900

    Apply search bar css

--- src/pages/Lists/components/SearchBar.js
+++ src/pages/Lists/components/SearchBar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./SearchBar.css";
 
 const SearchBar = () => {
   return (
     <div className="search-bar">
-      <input className="search-bar__input" type="text" />
+      <input
+        className="search-bar__input input blue-bold-outline"
+        type="text"
+      />
+      <div className="search-bar__submit">
+        <i className="fas fa-search search__btn" />
+      </div>
     </div>
   );
 };
