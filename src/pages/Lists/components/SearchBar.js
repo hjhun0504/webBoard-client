b37f6187cfc8a227d041c4867a6675c02339001b
hjhun-Macmini <hjhun0504@gmail.com>
2020-01-22 11:37:09 +0900

    Refactor SearchBar component to class component

--- src/pages/Lists/components/SearchBar.js
+++ src/pages/Lists/components/SearchBar.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./SearchBar.css";
 
-const SearchBar = () => {
-  return (
-    <div className="search-bar">
-      <input
-        className="search-bar__input input blue-bold-outline"
-        type="text"
-      />
-      <div className="search-bar__submit">
-        <i className="fas fa-search search__btn" />
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          className="search-bar__input input blue-bold-outline"
+          type="text"
+        />
+        <div className="search-bar__submit">
+          <i className="fas fa-search search__btn" />
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default SearchBar;
+    );
+  }
+}
